0c452faa9710f62ba9b68e62c42d65d994d44c03
uniqweber <uniqwebersocial@gmail.com>
2025-03-06 15:17:20 +0600

    smooth scroll added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,22 @@
+import { useEffect } from "react";
 import Footer from "../../components/shared/Footer";
 import AppleChip from "./homeComp/AppleChip";
 import Features from "./homeComp/Features";
 import Hero from "./homeComp/Hero";
 import Highlights from "./homeComp/Highlights";
 import Model from "./homeComp/Model";
+import Lenis from "lenis";
 
 const Home = () => {
+   useEffect(() => {
+     const lenis = new Lenis();
+     function update(time) {
+       lenis.raf(time);
+       requestAnimationFrame(update);
+     }
+     requestAnimationFrame(update);
+     return () => cancelAnimationFrame(update);
+   }, []);
   return (
     <main>
       <Hero />
